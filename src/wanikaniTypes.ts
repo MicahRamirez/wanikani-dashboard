f5e679922f47064c6574e3abb1a0637d163fc48b
MicahRamirez <xaviermicahramirez@utexas.edu>
2020-02-21 19:06:01 +0900

    working on new api calls

--- src/wanikaniTypes.ts
+++ src/wanikaniTypes.ts
@@ -14,3 +14,39 @@ export interface Reset {
   target_level: number; // The user's level after the reset, from 1 to 60. It must be less than or equal to original_level.
   confirmed_at: string; //Timestamp when the user confirmed the reset.
 }
+
+export interface AuxillaryMeaning {
+  meaning: string; // A singular subject meaning.
+  type: string; // Either whitelist or blacklist. When evaluating user input, whitelisted meanings are used to match for correctness. Blacklisted meanings are used to match for incorrectness.
+}
+
+	Array of objects	
+	String	
+created_at	Date	Timestamp when the subject was created.
+document_url	String	A URL pointing to the page on wanikani.com that provides detailed information about this subject.
+hidden_at	Date	Timestamp when the subject was hidden, indicating associated assignments will no longer appear in lessons or reviews and that the subject page is no longer visible on wanikani.com.
+lesson_position	Integer	The position that the subject appears in lessons. Note that the value is scoped to the level of the subject, so there are duplicate values across levels.
+level	Integer	The level of the subject, from 1 to 60.
+meaning_mnemonic	String	The subject's meaning mnemonic.
+meanings	Array of objects	The subject meanings. See table below for the object structure.
+slug	String	The string that is used when generating the document URL for the subject. Radicals use their meaning, downcased. Kanji and vocabulary use their characters.
+export interface Subject {
+  auxiliary_meanings: AuxillaryMeaning[]; // Collection of auxiliary meanings. See table below for the object structure.
+  characters: string; // The UTF-8 characters for the subject, including kanji and hiragana.
+}
+
+export interface Assignment {
+  available_at: string | null; // Timestamp when the related subject will be available in the user's review queue.
+  burned_at: string | null; // Timestamp when the user reaches SRS stage 9 the first time.
+  created_at: string; // Timestamp when the assignment was created.
+  hidden: boolean; // Indicates if the associated subject has been hidden, preventing it from appearing in lessons or reviews.
+  passed_at: string | null; // Timestamp when the user reaches SRS stage 5 for the first time.
+  passed: boolean; // The boolean equivalent of passed_at
+  resurrected_at: string | null; // Timestamp when the subject is resurrected and placed back in the user's review queue.
+  srs_stage_name: string; //	The stage name associated with the srs_stage.
+  srs_stage: number; //	The current SRS stage interval, from 0 to 9.
+  started_at: string | null; //	Timestamp when the user completes the lesson for the related subject.
+  subject_id: number; // Unique identifier of the associated subject.
+  subject_type: string; // The type of the associated subject, one of: kanji, radical, or vocabulary.
+  unlocked_at: string | null; // The timestamp when the related subject has its prerequisites satisfied and is made available in lessons.
+}
