008f799a79fb59601ee89eaa934d0d33629cc8cb
MicahRamirez <xaviermicahramirez@utexas.edu>
2020-03-01 17:22:12 +0900

    thinking about optimal time

--- src/Projections.tsx
+++ src/Projections.tsx
@@ -8,7 +8,7 @@ import {
   analyzeLevelProgressions,
   analyzeResetData
 } from "./levelUpProjectionUtils";
-import { useWKApi } from "./useWKApi";
+import { useWKApi, unwrapCollectionWrapper } from "./useWKApi";
 import { LevelProgression, Reset, Subject, Assignment } from "./wanikaniTypes";
 import {
   LEVEL_PROGRESSIONS_API_URL,
@@ -21,6 +21,64 @@ import {
   RESETS_LOCAL_STORAGE_KEY
 } from "./constants";
 import { LevelUpChart } from "./LevelUpChart";
+import { WanikaniCollectionWrapper } from "./localStorageUtils";
+
+/**
+ * Upon leveling up all radicals are assigned and a portion of kanji are unlocked
+ * in order to determine levelu
+ * @param wrappedLevelUpAssignments
+ * @param wrappedKanjiSubjects
+ */
+const calculateFastestLevelUpTime = (
+  wrappedLevelUpAssignments: WanikaniCollectionWrapper<Assignment>[],
+  wrappedKanjiSubjects: WanikaniCollectionWrapper<Subject>[]
+) => {
+  if (!wrappedKanjiSubjects || !wrappedLevelUpAssignments) {
+    return;
+  }
+  // levelUpAssignments are assignments that are required to be guru'ed to level up
+  const levelUpAssignments = unwrapCollectionWrapper(wrappedLevelUpAssignments)
+    .filter(elem => elem.subject_type !== "vocabulary")
+    .reduce<{ [subjectType: string]: Assignment[] }>(
+      (prev, currentObj) => {
+        prev[currentObj.subject_type].push(currentObj);
+        return prev;
+      },
+      { kanji: [], radical: [] }
+    );
+  const kanjiSubjects = unwrapCollectionWrapper(wrappedKanjiSubjects);
+  const levelUpRequirement = Math.ceil(kanjiSubjects.length * 0.9);
+  // when there aren't enough kanji assignments to level up we need to look at radicals (harder case)
+  if (levelUpAssignments.kanji.length < levelUpRequirement) {
+    // look at each radical and its srs level
+    // for an srs level what's the remaining time to completion (in seconds)
+    // radical -> remaining time left
+    //         -> kanji unlocked at guru
+    // which radicals are the closest to level up
+    // so take all radicals closest to level up
+    //    with these do they unlock enough kanji to meet the level requirement? if yes
+    // upon completion
+  } else {
+    // we have enough kanji unlocked start at the highest srs level
+    // group all kanji by srs level and sort by available at in each grouping
+    // kanjiGuruedSoFar: number
+    // totalRequiredTime: number (seconds?)
+    // for srs levels starting at GURU - 1 to Apprentice 1:
+    //    timeToAddForSrsLevel = 0
+    //    for each kanji in a srs level
+    //      set potentialTime to currentKanji.available_at - now
+    //      kanjiGuruedSoFar + 1;
+    //      if kanjiGuruedSoFar < levelUpRequirement
+    //        subtract
+    //
+    // add timeSoFar + time
+    // at Guru - 1 levels add up number of kanji. Does current Guru'ed kanji + Guru - 1 level kanji >= level requirement?
+  }
+
+  console.log("required kanji for level up", levelUpRequirement);
+  console.log("subjects", kanjiSubjects);
+  console.log(levelUpAssignments);
+};
 
 const useStyles = makeStyles(_ => ({
   root: {
@@ -78,7 +136,7 @@ export const Projections = ({ apiKey }: { apiKey: string }) => {
     apiKey
   );
   const [
-    { data: currentKanjiAssignments, isLoading: assignmentDataIsLoading }
+    { data: levelUpAssignments, isLoading: assignmentDataIsLoading }
   ] = useWKApi<Assignment>(
     ASSIGNMENTS_URL,
     {
@@ -96,7 +154,11 @@ export const Projections = ({ apiKey }: { apiKey: string }) => {
     apiKey
   );
   const classes = useStyles();
-
+  const fastestLevelUpTime = calculateFastestLevelUpTime(
+    levelUpAssignments,
+    currentKanjiSubjects
+  );
+  console.log(fastestLevelUpTime);
   if (
     isLoading ||
     resetDataIsLoading ||
@@ -104,7 +166,7 @@ export const Projections = ({ apiKey }: { apiKey: string }) => {
     subjectDataIsLoading ||
     !data ||
     !resetData ||
-    !currentKanjiAssignments ||
+    !levelUpAssignments ||
     !currentKanjiSubjects ||
     !formattedDataWithProjections
   ) {
